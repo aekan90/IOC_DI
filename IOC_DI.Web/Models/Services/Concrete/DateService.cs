61c31aeb983c19d672751041071a2d69f8c5f6b6
Atıf Ertugrul Kan <aekan90@hotmail.com>
2022-09-20 10:00:15 +0300

    .

--- IOC_DI.Web/Models/Services/Concrete/DateService.cs
+++ IOC_DI.Web/Models/Services/Concrete/DateService.cs
@@ -1,6 +1,9 @@
-﻿namespace IOC_DI.Web.Models.Services.Concrete
+﻿using IOC_DI.Web.Models.Services.Abstract;
+
+namespace IOC_DI.Web.Models.Services.Concrete
 {
-    public class DateService
+    public class DateService : ISingletonDateService, IScopedDateService, ITransientDateService
     {
+        public DateTime GetDateTime { get; } = DateTime.Now;
     }
 }
