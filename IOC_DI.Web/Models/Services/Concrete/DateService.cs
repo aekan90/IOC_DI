943f9cecfc37053b5bf6a34dc679aceda047755f
Atıf Ertugrul Kan <aekan90@hotmail.com>
2022-09-20 15:23:25 +0300

    .

--- IOC_DI.Web/Models/Services/Concrete/DateService.cs
+++ IOC_DI.Web/Models/Services/Concrete/DateService.cs
@@ -5,5 +5,12 @@ namespace IOC_DI.Web.Models.Services.Concrete
     public class DateService : ISingletonDateService, IScopedDateService, ITransientDateService
     {
         public DateTime GetDateTime { get; } = DateTime.Now;
+
+        private readonly ILogger<DateService> _logger;
+        public DateService(ILogger<DateService> logger)
+        {
+            _logger = logger;
+            _logger.LogWarning("DateService constructor'ına girdi");
+        }
     }
 }
--- IOC_DI.Web/Program.cs
+++ IOC_DI.Web/Program.cs
@@ -1,7 +1,14 @@
+using IOC_DI.Web.Models.Services.Abstract;
+using IOC_DI.Web.Models.Services.Concrete;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<ISingletonDateService, DateService>(); // her requeste 1 nesne örneði
+builder.Services.AddScoped<ISingletonDateService, DateService>();  // her requeste yeni 1 nesne örneði
+builder.Services.AddTransient<ISingletonDateService, DateService>(); // her requestteki her isteðe 1 nesne örneði
 
 var app = builder.Build();
 
