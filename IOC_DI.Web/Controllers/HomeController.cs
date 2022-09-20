d5dadc0a7a467f9ebdfc7b1ded8c30519243b6f4
Atıf Ertugrul Kan <aekan90@hotmail.com>
2022-09-20 15:31:27 +0300

    .

--- IOC_DI.Web/Controllers/HomeController.cs
+++ IOC_DI.Web/Controllers/HomeController.cs
@@ -1,20 +1,24 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using IOC_DI.Web.Models;
+using System.Security.AccessControl;
+using IOC_DI.Web.Models.Services.Abstract;
 
 namespace IOC_DI.Web.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly ISingletonDateService _singletonDateService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ISingletonDateService singletonDateServiceConst)
     {
-        _logger = logger;
+        _singletonDateService = singletonDateServiceConst;
     }
 
-    public IActionResult Index()
+    public IActionResult Index([FromServices]ISingletonDateService singletonDateServiceMetot)
     {
+        ViewBag.time1 = _singletonDateService.GetDateTime.TimeOfDay.ToString();
+        ViewBag.time2 = singletonDateServiceMetot.GetDateTime.TimeOfDay.ToString(); 
         return View();
     }
 
